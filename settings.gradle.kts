a9d1b8f6c9f8a606374d1cf74556147230aa39cd
Giovani Vitor <giovani.vitor@timwetech.com>
2023-08-16 17:27:08 -0500

    Esteira CI-CD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { setUrl("https://jitpack.io") }
         maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-        //maven(url = uri("https://oss.sonatype.org/content/repositories/snapshots/"))
     }
 }
 
